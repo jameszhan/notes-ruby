dca97f1e10887031e7e5211163c3ee6b1bd08bce
James Zhan <zhiqiangzhan@gmail.com>
2018-02-28 11:21:30 +0800

    add instance variables docs.

--- 03-ruby-syntax/dynamic_scope.pl
+++ 03-ruby-syntax/dynamic_scope.pl
@@ -3,8 +3,15 @@ sub invoker {
     &func();
 }
 
+sub invoker2 {
+    local $x = 'invoker2';
+    &func();
+}
+
 $x = 'global';
 sub func {
     print "$x\n";
 }
-invoker()
+
+invoker();
+invoker2();
